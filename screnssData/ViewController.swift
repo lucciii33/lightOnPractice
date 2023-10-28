186f3547c49a8f7d21f2ff587d7666a3a4d3f6ad
Angelo Maiele <angelomaiele@gamil.com>
2023-10-28 09:50:57 -0400

    Initial Commit

---
+++ screnssData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screnssData
+//
+//  Created by Angelo on 10/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
