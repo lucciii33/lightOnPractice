410694196bf89225d3fcd3440b661468fb3ca1df
Angelo Maiele <angelomaiele@gamil.com>
2023-10-28 11:04:06 -0400

    some cool light on and off

--- screnssData/ViewController.swift
+++ screnssData/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+//    @IBOutlet weak var lightButton: UIButton!
+    
+    var lightOn = true
+    fileprivate func updateUI() {
+            view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
 }
 
